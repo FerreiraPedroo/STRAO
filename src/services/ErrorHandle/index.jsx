2e1cb475174ad6f85536e63a861d4ed65d5a9c67
Pedro Henrique de Assis Ferreira <73146680+FerreiraPedroo@users.noreply.github.com>
2022-10-02 12:34:12 -0300

    update add errorhandle

---
+++ src/services/ErrorHandle/index.jsx
@@ -0,0 +1,22 @@
+const codStatusErrors = {
+  401: "Remover login",
+};
+
+export const errorHandle = (_data, setNotificationModal, handleContext) => {
+  const responseError = _data;
+
+  if (!responseError) {
+    return setNotificationModal({ message: "Erro na rede" });
+  }
+  switch (responseError.codStatus) {
+    case 401:
+      handleContext.removeLoginData();
+      break;
+    case 422:
+      setNotificationModal(responseError);
+      break;
+    default:
+      setNotificationModal({ codStatus: 0, message: `ERRO DESCONHECIDO: ${responseError.message}` });
+      break;
+  }
+};
