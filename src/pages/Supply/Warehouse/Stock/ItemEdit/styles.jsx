9ddcb66486768a46bfa1d6365d1f74cd430e9115
Pedro Henrique de Assis Ferreira <73146680+FerreiraPedroo@users.noreply.github.com>
2024-01-17 23:25:52 -0300

    update warehouse

---
+++ src/pages/Supply/Warehouse/Stock/ItemEdit/styles.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	width: 100%;
+	padding: 16px 16px;
+	display: flex;
+	justify-content: start;
+	align-content: start;
+	flex-wrap: wrap;
+	gap: 8px;
+	background-color: #f5f3f0;
+`;
---
+++ src/pages/Supply/Warehouse/Stock/index.jsx
@@ -0,0 +1,155 @@
+import React, { useState, useContext, useEffect } from "react";
+
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { api } from "services/api";
+import { NotificationModal } from "component/Notification/modal";
+
+import { PageList } from "component/container/PageList";
+import { PageFilter } from "component/container/PageFilter";
+import { PageAction } from "component/container/PageAction";
+import { PageTitle } from "component/container/PageTitle";
+import * as S from "./styles";
+
+export function SupplyWarehouseStock() {
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const [notification, setNotification] = useState(null);
+
+	const [pageLoading, setPageLoading] = useState(true);
+	const [searchLoading, setSearchLoading] = useState(true);
+
+	const [itemsList, setItemsList] = useState([]);
+	const [itemSelected, setItemSelected] = useState(null);
+
+	const [listColumns] = useState([
+			{
+				title: "ID",
+				htmlName: "_id",
+				size: 128,
+				minSize: 128,
+				maxSize: 220,
+				align: "center"
+			},
+			{ title: "nome", htmlName: "name", size: 128, minSize: 128, maxSize: "100%", align: "start" },
+			{
+				title: "Quantidade",
+				htmlName: "amount",
+				size: 104,
+				minSize: 104,
+				maxSize: 104,
+				align: "center"
+			}
+	]);
+	
+	// const [listActions] = useState([
+	// 	{
+	// 		name: "Ultimas movimentações",
+	// 		typeStyle: "document",
+	// 		show: true,
+	// 		action: () => navigate("/supply/item/register")
+	// 	},
+	// 	// {
+	// 	// 	name: "Editar",
+	// 	// 	typeStyle: "edit",
+	// 	// 	show: true,
+	// 	// 	action: (employee) =>
+	// 	// 		navigate("/rh/employee/edit", {
+	// 	// 			state: { dataId: employee._id }
+	// 	// 		})
+	// 	// }
+	// ]);
+
+	// const [filters] = useState([
+	// 	// {
+	// 	// 	type: "select",
+	// 	// 	htmlName: "contract",
+	// 	// 	htmlPlaceholder: "Contrato",
+	// 	// 	showInfo: true,
+	// 	// 	defaultOption: 0,
+	// 	// 	options: Object.values(userData.contractsInfo).reduce(
+	// 	// 		(acc, cur) => {
+	// 	// 			acc.push({ name: cur.name, value: cur._id });
+	// 	// 			return acc;
+	// 	// 		},
+	// 	// 		[{ name: "Todos", value: "" }]
+	// 	// 	)
+	// 	// },
+	// 	{
+	// 		type: "select",
+	// 		htmlName: "findBy",
+	// 		htmlPlaceholder: "Procurar por",
+	// 		showInfo: true,
+	// 		defaultOption: 0,
+	// 		options: [
+	// 			{ name: "Nome", value: "name" },
+	// 			{ name: "Identificação", value: "identification" }
+	// 		]
+	// 	},
+	// 	{
+	// 		type: "text",
+	// 		htmlName: "findText",
+	// 		htmlPlaceholder: "Procurar",
+	// 		showInfo: true
+	// 	}
+	// ]);
+
+
+	useEffect(() => {
+		async function getItems() {
+			setPageLoading(true);
+
+			try {
+				const response = await api.get(`/supply/warehouse/stock/${location.state._id}/items`);
+
+				setItemsList(response.data.data);
+				setPageLoading(false);
+			} catch (error) {
+				setNotification({
+					theme: "fail",
+					message:
+						(error.response && error.response.data.message) ?? "Erro ao obter os dados do estoque.",
+					setNotification: setNotification
+				});
+
+				setPageLoading(false);
+			}
+		}
+		getItems();
+	}, []);
+
+	return (
+		<S.Container>
+			{notification && (
+				<NotificationModal
+					theme={notification.theme}
+					message={notification.message}
+					setNotification={setNotification}
+				/>
+			)}
+
+			<PageTitle
+				title="Estoque"
+				backUrl={"/supply/warehouse/stock/list"}
+				subTitle={"Todos os items do estoque"}
+			/>
+
+			<S.StockName>
+			{location.state.name}
+			</S.StockName>
+
+			{/* <PageAction actionsData={listActions} dataSelected={null} loading={pageLoading} /> */}
+
+			{/* <PageFilter filtersData={filters} getFiltersSelected={getItems} loading={searchStatus} /> */}
+
+			<PageList
+				listData={itemsList}
+				columns={listColumns}
+				setDataSelected={setItemSelected}
+				loading={pageLoading}
+			/>
+		</S.Container>
+	);
+}
