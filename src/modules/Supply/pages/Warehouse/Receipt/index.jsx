7aa0e421ef7a1987fbe3688a1b4196c59e529a02
Pedro Henrique de Assis Ferreira <73146680+FerreiraPedroo@users.noreply.github.com>
2025-01-06 22:03:56 -0300

    update supply

---
+++ src/modules/Supply/pages/Warehouse/Receipt/index.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useContext, useEffect } from "react";
+
+import { useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { api } from "services/api";
+import { NotificationModal } from "component/Notification/modal";
+
+import { PageList } from "component/container/PageList";
+import { PageAction } from "component/container/PageAction";
+import { PageTitle } from "component/container/PageTitle";
+import * as S from "./styles";
+
+export function SupplyWarehouseReceipt() {
+	const navigate = useNavigate();
+
+	const [notification, setNotification] = useState(null);
+
+	const [pageLoading, setPageLoading] = useState(true);
+	const [searchLoading, setSearchLoading] = useState(true);
+
+	const [itemsList, setItemsList] = useState([]);
+	const [itemSelected, setItemSelected] = useState(null);
+
+	const [listColumns] = useState([
+		{
+			name: "Id",
+			htmlName: "_id",
+			minSize: 96,
+			maxSize: 128,
+			align: "center"
+		},
+		{ name: "Nome", htmlName: "name", size: 256, minSize: 256, maxSize: "auto" },
+		{ name: "Quantidade", htmlName: "amount", size: 256, minSize: 128, maxSize: 128, align: "center" }
+		// { name: "E-mail", htmlName: "email", size: 0 }
+	]);
+	
+	const [listActions] = useState([
+		{
+			name: "Novo recebimento",
+			typeStyle: "add",
+			show: true,
+			action: () => navigate("/supply/warehouse/shipping/receive")
+		},
+	]);
+
+	useEffect(() => {
+		async function getItems() {
+			setPageLoading(true);
+
+			try {
+				const response = await api.get(`/supply/warehouse/shippings`);
+
+				setItemsList(response.data.data);
+				setPageLoading(false);
+			} catch (error) {
+				setNotification({
+					theme: "fail",
+					message:
+						(error.response && error.response.data.message) ?? "Erro ao obter os dados dos recebimentos.",
+					setNotification: setNotification
+				});
+
+				setPageLoading(false);
+			}
+		}
+		getItems();
+	}, []);
+
+	return (
+		<S.Container>
+			{notification && (
+				<NotificationModal
+					theme={notification.theme}
+					message={notification.message}
+					setNotification={setNotification}
+				/>
+			)}
+
+			<PageTitle
+				title="Recebimento"
+				backUrl={"/supply"}
+				subTitle={"Registre o recebimento de materiais selecionando o estoque e a origem do material."}
+			/>
+
+			<PageAction actionsData={listActions} dataSelected={null} loading={pageLoading} />
+
+			{/* <PageFilter filtersData={filters} getFiltersSelected={getItems} loading={searchStatus} /> */}
+
+			<PageList
+				listData={itemsList}
+				columns={listColumns}
+				setDataSelected={setItemSelected}
+				loading={pageLoading}
+			/>
+		</S.Container>
+	);
+}
---
+++ src/modules/Supply/pages/Warehouse/StockList/index.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { api } from "services/api.js";
+import { NotificationModal } from "component/Notification/modal.jsx";
+
+import { PageAction } from "component/container/PageAction/index.jsx";
+import { PageCardList } from "component/container/PageCardList/index.jsx";
+import { PageTitle } from "component/container/PageTitle/index.jsx";
+
+import { CreateStockModal } from "./CreateStockModal/index.jsx";
+
+import * as S from "./styles.jsx";
+import { useDispatch } from "react-redux";
+
+export function SupplyWarehouseStockList() {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const [pageLoading, setPageLoading] = useState(true);
+	const [notification, setNotification] = useState(null);
+
+	const [itemList, setItemList] = useState(null);
+
+	async function getItemList() {
+		setPageLoading(true);
+
+		try {
+			const response = await api.get("/supply/warehouse/stock/list");
+
+			if (response.data.codStatus == 200) {
+				const itemList = response.data.data.map((item) => {
+					return { ...item, url_path: "/supply/warehouse/stock" };
+				});
+
+				setItemList(itemList);
+			} else if (response.data.codStatus === 401) {
+				dispatch(changeLoginReseted(true));
+			}
+		} catch (error) {
+			setNotification({
+				theme: "fail",
+				message:
+					error.response && error.response.data.message ? error.response.statusText : error.message,
+				setNotification: setNotification
+			});
+		}
+
+		setPageLoading(false);
+	}
+
+	useEffect(() => {
+		getItemList();
+	}, []);
+
+	return (
+		<S.Container>
+			{notification && (
+				<NotificationModal
+					theme={notification.theme}
+					message={notification.message}
+					setNotification={setNotification}
+				/>
+			)}
+
+			<PageTitle
+				title="Lista de estoque"
+				subTitle="Selecione um estoque para visualizar."
+				backUrl={"/supply"}
+				loading={pageLoading}
+			/>
+
+			<PageCardList listData={itemList} pageLoading={pageLoading} />
+		</S.Container>
+	);
+}
